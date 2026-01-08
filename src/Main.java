58fe634fea75a3b7484eb7d43ab6b8bedde43125
jordonjones <jordon01jones@gmail.com>
2026-01-08 16:41:54 -0600

    function21

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+//  **Reverse String**
+//    Write a function to reverse a (using loop). `String`
+
 public class Main {
+    public static String reverse(String s){
+        String rev = "" ;
+        for(int i = s.length() - 1 ; i >= 0 ; i--){
+            // need to take the index and make it go backwards
+             char c = s.charAt(i) ;
+             rev = rev + c ;
+        }
+
+        return rev;
+
+    }
+
+    public static void main(String [] args){
+        System.out.println(reverse("booty")) ;
+    }
+
 }
